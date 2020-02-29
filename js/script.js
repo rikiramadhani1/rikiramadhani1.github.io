fddbc438afa8259e3d30fb10a8286e3c6b0182a9
rikiramadhani1 <rikiramadhani08@gmail.com>
2020-02-29 13:52:23 +0700

    add parallax

--- js/script.js
+++ js/script.js
@@ -13,3 +13,37 @@ $('.page-scroll').on('click', function(e) {
 
     e.preventDefault();
 });  
+
+
+// parallax
+// about
+$(window).on('load', function() {
+    $('.left').addClass('pView')
+    $('.right').addClass('pView')
+});
+
+$(window).scroll(function() {
+    var wScroll = $(this).scrollTop();
+    
+    // jumbotron
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    })
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    })
+
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+    })
+
+    // portfolio
+    if (wScroll > $('.portfolio').offset().top - 250) {
+        $('.portfolio .thumbnail').each(function(i) {
+            setTimeout(function() {
+                $('.portfolio .thumbnail').eq(i).addClass('view')
+            }, 200 * i)
+        });
+    }
+});
